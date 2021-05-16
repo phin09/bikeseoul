c9f32eb103c01b7a02361d2681e279e1a75a7c47
junha6316 <junha6316@gmail.com>
2021-05-16 15:07:27 +0900

    temporarily commit

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'user'
 urlpatterns = [
-    path("login/", views.login, name="login")
+    path("login/", views.login, name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,28 +1,36 @@
 import os
 import jwt
+import json
 import bcrypt
+import pbkdf2
 
 from django.shortcuts import render
+from django.contrib.auth.hashers import check_password
 from django.http import JsonResponse
 
 from .models import User
 
-def login(self, request):
+def login(request):
+    print(request.body)
     data = json.loads(request.body)
     try:
         input_account  = data['account']
         input_password = data['password'].encode('utf-8')
+
     except Exception as error_msg:
         return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
 
     try:
-        user = User.objects.get(email=input_account)
+        user = User.objects.get(username=input_account)
     except User.DoesNotExist:
         return JsonResponse({'message': 'INVALID_USER'}, status = 400)
 
-    if bcrypt.checkpw(input_password, user.password.encode('utf-8')):
-        SECRET = os.environ.get('DJANGO_SECRET_KEY')
-        ALGORITHM = os.environ.get('ALGORITHM')
-        access_token = jwt.encode({'id':user.id}, SECRET, algorithm=ALGORITHM)
+    # if bcrypt.checkpw(input_password, user.password.encode('utf-8')):
+    #     SECRET = os.environ.get('DJANGO_SECRET_KEY')
+    #     ALGORITHM = os.environ.get('ALGORITHM')
+    #     access_token = jwt.encode({'id':user.id}, SECRET, algorithm=ALGORITHM)
+
+    if check_password(input_password, user.password):
         return JsonResponse({'message': 'SUCCESS', 'access_token': access_token.decode('utf-8')}, status = 200)
+
     return JsonResponse({'message': 'INVALID PASSWORD OR ACCOUNT'}, status = 400)
