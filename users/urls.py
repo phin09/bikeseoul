7afa3798fd667d92435120f5f5b8f4e7bd32de0d
junha6316 <junha6316@gmail.com>
2021-05-19 14:56:43 +0900

    CREATE: LogIn and Logout Feature

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
-    path("login/", views.login, name="login"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout")
 ]
--- users/views.py
+++ users/views.py
@@ -1,22 +1,24 @@
 import os
+from re import A
 import jwt
 import json
+
+from jwt import algorithms
 import bcrypt
 import pbkdf2
 
 from django.shortcuts import render
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth import logout
 from django.http import JsonResponse
 
 from .models import User
 
-def login(request):
-    print(request.body)
+def login_view(request):
     data = json.loads(request.body)
     try:
         input_account  = data['account']
         input_password = data['password'].encode('utf-8')
-
     except Exception as error_msg:
         return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
 
@@ -25,12 +27,33 @@ def login(request):
     except User.DoesNotExist:
         return JsonResponse({'message': 'INVALID_USER'}, status = 400)
 
-    # if bcrypt.checkpw(input_password, user.password.encode('utf-8')):
-    #     SECRET = os.environ.get('DJANGO_SECRET_KEY')
-    #     ALGORITHM = os.environ.get('ALGORITHM')
-    #     access_token = jwt.encode({'id':user.id}, SECRET, algorithm=ALGORITHM)
-
     if check_password(input_password, user.password):
-        return JsonResponse({'message': 'SUCCESS', 'access_token': access_token.decode('utf-8')}, status = 200)
-
+        SECRET = os.environ.get('DJANGO_SECRET_KEY')
+        ALGORITHM = os.environ.get('ALGORITHM')
+        access_token = jwt.encode({'id':user.id}, SECRET, algorithm=ALGORITHM)
+        request.session[user.id] = 'login'
+        return JsonResponse({'message': 'SUCCESS', 'access_token': access_token}, status = 200)
     return JsonResponse({'message': 'INVALID PASSWORD OR ACCOUNT'}, status = 400)
+
+
+def logout_view(request):
+    data = json.loads(request.body)
+    access_token = data.get('access_token', None)
+    if not access_token:
+        return JsonResponse({'message': 'Empty Token'}, status = 400)
+    
+    SECRET = os.environ.get('DJANGO_SECRET_KEY')
+    ALGORITHM = os.environ.get('ALGORITHM')
+
+    try:
+        payload = jwt.decode(access_token, SECRET, algorithms=[ALGORITHM])
+    except jwt.exceptions.InvalidSignatureError:
+        return JsonResponse({'message':'Signature Verification Failed'}, status=401)
+
+    user_id = payload['id']
+    try:
+        del request.session[str(user_id)]
+    except KeyError:
+        return JsonResponse({'messsage':"User is already logged out"} , status=200)
+
+    return JsonResponse({'message':'User is logged out Successfully'}, status=200)
